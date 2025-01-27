43ff0b3093ee74df7487888f749d5d49fddadd86
Alay <62489788+alaykumar@users.noreply.github.com>
2025-01-27 02:17:49 -0500

    Update PrivateRoute.js

--- frontend/src/utils/PrivateRoute.js
+++ frontend/src/utils/PrivateRoute.js
@@ -1,15 +1,3 @@
-/*import { useContext } from "react";
-import { Route, Redirect } from "react-router-dom";
-import AuthContext from "../context/AuthContext"
-
-const PrivateRoute = ({children, ...rest}) => {
-    let {user} = useContext(AuthContext)
-    return <Route {...rest}>{!user ? <Redirect to="/login" /> : children}</Route>
-};
-
-export default PrivateRoute*/
-
-// utils/PrivateRoute.js
 import React, { useContext } from "react";
 import { Route, Redirect } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
